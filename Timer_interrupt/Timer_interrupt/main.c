655708945c0bf0b124f304bd9d74fadc3a449082
AbdelrahmanEmadeldeen <47720327+AbdelrahmanEmadeldeen@users.noreply.github.com>
2019-02-21 11:24:42 +0200

    Add files via upload

---
+++ Timer_interrupt/Timer_interrupt/main.c
@@ -0,0 +1,31 @@
+/*
+ * GPIO.c
+ *
+ * Created: 2/13/2019 10:41:19 AM
+ * Author : AVE-LAP-44
+ */ 
+
+
+
+    /* Replace with your application code */
+ 
+#include "Commen\commen.h"
+#include "Dio\DIO_prog.h"
+#include "Timer\Timer.h"
+
+
+
+ int main(void)
+ {   DIO_SetPinDirection(PINB4,HIGH);
+	 	 DIO_WritePin(PINB4,HIGH);
+
+	 while(1){
+	 DIO_WritePin(PINB4,HIGH);
+	 timer_mdelay_interrupt(TIME_1000MS);
+	 DIO_WritePin(PINB4,LOW);
+	 timer_mdelay_interrupt(TIME_1000MS);
+	 }
+	 return 0;
+ }
+
+
